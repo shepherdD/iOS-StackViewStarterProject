a03cf6e4fd786764b0b69273248ef4570d24e719
wudi <wudi@wudideMacBook-Pro.local>
2015-07-19 16:41:28 +0800

    test

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController
     @IBOutlet weak var verticalStackView: UIStackView!
     @IBOutlet weak var horizontalStackView: UIStackView!
     
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
